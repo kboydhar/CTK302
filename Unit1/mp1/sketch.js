107b63870dbaa466796d78efabe6bd0841fb8fa7
kboydhar <kbboydh@ilstu.edu>
2021-09-09 23:07:29 -0500

    mp1

--- Unit1/mp1/sketch.js
+++ Unit1/mp1/sketch.js
@@ -2,31 +2,63 @@ function setup() {
   createCanvas(1000, 800);
   rectMode(CENTER);
   ellipseMode(CENTER);
-
 }
-
-
 
 function draw() {
 
-  background(100);
+  background(255, 204, 0);
 
 
   if (mouseIsPressed) {
     // stuff that shows when the mouse is pressed
+    fill('black')
+    rect(115, 100, 200, 200) ;
+    rect(115, 100, 200, 200) ;
+    triangle(150, 250, 100, 150, 100, 100);
+    ellipse(582, 542, 200, 200) ;
+    rect(355, 279, 40, 73) ;
+    rect(355, 370, 200, 200);
+    rect(290, 475, 40, 150);
+    rect(410, 475, 40, 150);
+    rect(410, 555, 75, 15);
+    rect(286, 555, 75, 15);
+    rect(70, 550, 200, 20);
+    rect(160, 560, 50, 350);
+    ellipse(160, 375, 50, 50) ;
+     fill('white');
+  textSize(60)
+  text("OUCH!", 20, 70);
+     fill('black');
+  textSize(24)
+  text("He didn't listen", 315, 70);
 
   } else {
     // when the mouse isn't pressed!
+      fill(0);
+  textSize(24)
+  text("This is Ted. We told him not to push the red button.\n He is a robot designed \n to test instructions. ", 10, 30);
+    ellipse(355, 144, 200, 200) ;
+    rect(355, 279, 40, 73) ;
+    rect(355, 370, 200, 200);
+    rect(290, 475, 40, 150);
+    rect(410, 475, 40, 150);
+    rect(410, 555, 75, 15);
+    rect(286, 555, 75, 15);
+    rect(255, 330, 200, 20);
+
+    fill("red")
+    ellipse(160, 375, 50, 50) ;
+    fill ("black")
+    rect(160, 560, 50, 350);
 
   }
 
 
 
+
   // this shows mouse location - comment it out when you're done!
 
-  fill(0);
-  text(mouseX + ", " + mouseY, 40, 40);
-  text("This is my back story. \n ")
+
 
 }
 
@@ -39,3 +71,7 @@ function draw() {
 function mouseReleased() {
   print(mouseX + ", " + mouseY);
 }
+
+
+
+  
